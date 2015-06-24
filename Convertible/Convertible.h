1039325bbfd0909b7c608b0850e4796d41ccb8b5
bradhilton <brad.hilton.nw@gmail.com>
2015-06-24 10:51:45 -0600

    Initial Commit

---
+++ Convertible/Convertible.h
@@ -0,0 +1,19 @@
+//
+//  Convertible.h
+//  Convertible
+//
+//  Created by Bradley Hilton on 6/24/15.
+//  Copyright © 2015 Skyvive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Convertible.
+FOUNDATION_EXPORT double ConvertibleVersionNumber;
+
+//! Project version string for Convertible.
+FOUNDATION_EXPORT const unsigned char ConvertibleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Convertible/PublicHeader.h>
+
+
